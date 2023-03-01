d41229ab0884b2ceaf6989e1c7fd327f44fde705
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-03-01 18:25:38 +0300

    Test removeToDo method

--- tests/ToDo.test.js
+++ tests/ToDo.test.js
@@ -11,4 +11,9 @@ describe('ToDo Tests', () => {
     const itemsLength = todo.getListOfToDos().length;
     expect(itemsLength).toBe(2);
   });
+  test('todo.removeToDo() can remove an item', () => {
+    todo.removeToDo('1');
+    const itemsLength = todo.getListOfToDos().length;
+    expect(itemsLength).toBe(1);
+  });
 });
